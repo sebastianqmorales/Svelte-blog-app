5cced53c650c86973cfd01eb377c1a81ff2653e2
Sebastian <swebermorales@gmail.com>
2022-04-28 12:36:37 -0700

    styling about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,28 @@
-<a href="/">Go Back</a>
+<div class="container">
+	<a class="back-button" href="/">Go Back</a>
+	<h1>This is the about page</h1>
+	<p>This is my about page.</p>
+</div>
 
-<h1>This is the about page</h1>
+<style>
+	.container {
+		margin: 1rem;
+		padding: 2rem;
+	}
 
-<p>This is my about page.</p>
+	.back-button {
+		text-decoration: none;
+		color: #575899;
+		border: 1px solid #575899;
+		border-radius: 0.5rem;
+		padding: 0.1rem;
+	}
+	.back-button:hover {
+		font-weight: 600;
+		border: 2px solid #575899;
+	}
+
+	p {
+		margin-left: 0.25rem;
+	}
+</style>
